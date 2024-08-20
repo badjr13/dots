3b2b6424f9a1a544e6025cf2d4e3e89040a380c6
badjr13 <dorrance.bobby@gmail.com>
2024-08-20 08:24:38 -0400

    Dynamic Versioning

--- src/cli.rs
+++ src/cli.rs
@@ -7,7 +7,7 @@ use clap_builder::Command;
 pub fn root() -> Command {
     Command::new("dots")
         .author("badjr13")
-        .version("0.1.0")
+        .version(env!("CARGO_PKG_VERSION"))
         .about("A dead simple dotfile manager")
         .subcommand(track::command())
         .subcommand(deploy::command())
