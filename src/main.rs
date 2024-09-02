df2d289199d71f47db2d31a1e4c2b491b4a34e19
badjr13 <dorrance.bobby@gmail.com>
2024-09-01 23:33:57 -0400

    Get Matches

--- src/main.rs
+++ src/main.rs
@@ -1,6 +1,21 @@
 mod cli;
 
 fn main() {
-    let x = cli::root().get_matches();
-    dbg!(x);
+    let matches = cli::root().get_matches();
+
+    if let Some(track_match) = matches.subcommand_matches("track") {
+        dbg!(track_match);
+    }
+
+    if let Some(deploy_match) = matches.subcommand_matches("deploy") {
+        dbg!(deploy_match);
+    }
+
+    if let Some(destroy_match) = matches.subcommand_matches("destroy") {
+        dbg!(destroy_match);
+    }
+
+    if let Some(manifest_match) = matches.subcommand_matches("manifest") {
+        dbg!(manifest_match);
+    }
 }
