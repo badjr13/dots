ccf69e7eced0f28a9361934f9acc210a6ac5a722
badjr13 <dorrance.bobby@gmail.com>
2024-09-27 15:00:03 -0400

    Set path and list items

--- src/cli/init.rs
+++ src/cli/init.rs
@@ -13,12 +13,26 @@ pub fn command() -> Command {
 pub fn handle_matches(matches: &ArgMatches) -> Result<()> {
     if let Some(path) = matches.get_one::<String>("path") {
         if Path::new(path).exists() {
-            let mut absolute_path = fs::canonicalize(path)?;
-            absolute_path.push("manifest.toml");
-            fs::File::create(absolute_path)?;
+            let absolute_path = fs::canonicalize(path)?;
+            create_manifest(&absolute_path)?;
+            track_initial_items(&absolute_path)?;
         } else {
             println!("Please supply an existing path. '.' can be used to initalize the current working directory.");
         }
+    }
+    Ok(())
+}
+
+fn create_manifest(path: &Path) -> Result<()> {
+    let manifest_path = path.join("manifest.toml");
+    fs::File::create(&manifest_path)?;
+    Ok(())
+}
+
+fn track_initial_items(path: &Path) -> Result<()> {
+    let items = fs::read_dir(path)?;
+    for item in items {
+        dbg!(item?);
     }
     Ok(())
 }
