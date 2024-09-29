f9f93b2048178f7b6d9aa78419c941d970fc9fcc
badjr13 <dorrance.bobby@gmail.com>
2024-09-29 14:44:31 -0400

    Write tracked items to manifest.toml

--- src/cli/init.rs
+++ src/cli/init.rs
@@ -1,8 +1,11 @@
 use super::INIT;
 use clap_builder::{Arg, ArgMatches, Command};
 use color_eyre::eyre::Result;
-use std::fs;
+use std::fs::{canonicalize, read_dir, File, OpenOptions};
+use std::io::Write;
 use std::path::Path;
+
+const MANIFEST_FILE: &str = "manifest.toml";
 
 pub fn command() -> Command {
     Command::new(INIT)
@@ -13,26 +16,37 @@ pub fn command() -> Command {
 pub fn handle_matches(matches: &ArgMatches) -> Result<()> {
     if let Some(path) = matches.get_one::<String>("path") {
         if Path::new(path).exists() {
-            let absolute_path = fs::canonicalize(path)?;
-            create_manifest(&absolute_path)?;
-            track_initial_items(&absolute_path)?;
+            let absolute_path = canonicalize(path)?;
+            let manifest_file = create_manifest(&absolute_path)?;
+            track_initial_items(&absolute_path, &manifest_file)?;
         } else {
             println!("Please supply an existing path. '.' can be used to initalize the current working directory.");
         }
     }
+
     Ok(())
 }
 
-fn create_manifest(path: &Path) -> Result<()> {
-    let manifest_path = path.join("manifest.toml");
-    fs::File::create(&manifest_path)?;
-    Ok(())
+fn create_manifest(path: &Path) -> Result<File> {
+    let full_path = path.join(MANIFEST_FILE);
+
+    let manifest_file = OpenOptions::new()
+        .write(true)
+        .append(true)
+        .create(true)
+        .open(full_path)?;
+
+    Ok(manifest_file)
 }
 
-fn track_initial_items(path: &Path) -> Result<()> {
-    let items = fs::read_dir(path)?;
+fn track_initial_items(path: &Path, mut file: &File) -> Result<()> {
+    let items = read_dir(path)?;
+
     for item in items {
-        dbg!(item?);
+        let item = item?.path();
+        let item = item.as_path().to_str().unwrap_or_default();
+        writeln!(file, "{:?}", &item)?;
     }
+
     Ok(())
 }
